2f83f097d5a8b1a6b7c0eb0ceda3b1a4a5de5093
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2023-12-12 05:33:41 +0200

    Style update todolist

--- frontend/src/components/TodoList/TodoMangement/TodoItem.jsx
+++ frontend/src/components/TodoList/TodoMangement/TodoItem.jsx
@@ -1,58 +1,54 @@
 import { useState } from "react";
-import {setCompleted} from "../../../myAPIS"
+import { setCompleted } from "../../../myAPIS";
 import { useStore } from "../../../store/todoState";
-import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
-import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked";
 import { useUser } from "../../../store/authState";
 
-
-export default function TodoItems({items}) {
-  const {_id , title , completed , time , steps} = items;
-  const [completedState , setCompletedState] = useState(completed)
-  const setSidePanelItem = useStore(store => store.setSidePanelItem)
-  const sidePanelItem = useStore(store => store.sidePanelItem)
-  const {token} = useUser(user => user.user)
-
+export default function TodoItems({ items }) {
+  const { _id, title, completed, time, steps } = items;
+  const [completedState, setCompletedState] = useState(completed);
+  const setSidePanelItem = useStore((store) => store.setSidePanelItem);
+  const sidePanelItem = useStore((store) => store.sidePanelItem);
+  const { token } = useUser((user) => user.user);
 
   const handlePanelItem = () => {
-    setSidePanelItem(items)
-  }   
+    setSidePanelItem(items);
+  };
 
   const checkSidePanel = () => {
-    return _id === sidePanelItem._id
-  }
+    return _id === sidePanelItem._id;
+  };
   const handleClick = (e) => {
     e.stopPropagation();
-    setCompletedState(true)
-  }   
-  // global state 
+    setCompletedState(true);
+  };
+  // global state
   //----
   return (
-    <li className={`flex justify-between border-b-[1px] transition-all relative  duration-200  ease-in-out ${completedState ? "border-b-green-500  border-b-2 ele" : "border-b-[#ffffff48]"}
-      ${checkSidePanel() ? "bg-[#1c1e24] border-b-blue-500 border-b-4 shadow-sm shadow-transparent "  : "bg-[#22252c] hover:bg-[#252830]"}
-    h-16 m-1 cursor-pointer  transition-colors duration-100 ease-in-out`} onClick={() => handlePanelItem() } >
-        <div className="m-auto ml-2">{title}</div>
-        <div
+    <li
+      className={` li-item ${
+        completedState && "border-b-green-500  border-b-2 ele"
+      }  ${
+        checkSidePanel() &&
+        "bg-[#1c1e24] hover:bg-[#1c1e24] border-b-blue-500 border-b-4 shadow-sm shadow-transparent"
+      }`}
+      onClick={() => handlePanelItem()}
+    >
+      <div className="m-auto ml-2">{title}</div>
+      <div
         onClick={(e) => handleClick(e)}
-         className="my-auto mr-5 text-3xl transition-all duration-200 hover:text-4xl hover:text-green-300"
-         >{completedState ? <CheckCircleOutlineIcon fontSize="inherit"  className="text-5xl text-green-400 font "/> : <RadioButtonUncheckedIcon  fontSize="inherit" />  }</div>
+        className="my-auto mr-5 text-3xl transition-all duration-200 hover:text-4xl hover:text-green-300"
+      >
+        {completedState ? (
+          <CheckCircleOutlineIcon
+            fontSize="inherit"
+            className="text-5xl text-green-400 font "
+          />
+        ) : (
+          <RadioButtonUncheckedIcon fontSize="inherit" />
+        )}
+      </div>
     </li>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
