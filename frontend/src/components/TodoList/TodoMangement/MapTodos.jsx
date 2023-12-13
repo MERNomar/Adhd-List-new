c8589a8288d37437814b4ca16d3df0d41f76e17c
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2023-12-13 07:22:41 +0200

    update font to bold

--- frontend/src/components/TodoList/TodoMangement/MapTodos.jsx
+++ frontend/src/components/TodoList/TodoMangement/MapTodos.jsx
@@ -16,7 +16,7 @@ export default function AllTodos({ category }) {
       <ol>
         {/* This will check if there is not todos */}
         {arrayFilter.length === 0 ? (
-          <h3 className="m-auto mt-5 text-4xl text-center text-blue-300 animate-bounce">
+          <h3 className="m-auto mt-5 text-4xl font-bold text-center text-blue-300 animate-bounce">
             Add {category} Todos Now !{" "}
           </h3>
         ) : (
