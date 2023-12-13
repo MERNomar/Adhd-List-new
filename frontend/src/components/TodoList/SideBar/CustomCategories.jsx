f867a94e250f56a97ba60aaf1d61f1579c686d8b
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2023-12-13 10:45:01 +0200

    Update navItem to dynamic array

--- frontend/src/components/TodoList/SideBar/CustomCategories.jsx
+++ frontend/src/components/TodoList/SideBar/CustomCategories.jsx
@@ -5,8 +5,18 @@ import HomeSVG from "@mui/icons-material/HomeOutlined";
 import ImportantSVG from "@mui/icons-material/StarPurple500Outlined";
 import SadFaceSVG from "@mui/icons-material/SentimentDissatisfiedOutlined";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 export default function CustomCategories() {
+  // [hideWork , setHideWork] = useState()
+  // [hideWork , setHideWork] = useState()
+  // [hideWork , setHideWork] = useState()
+  const navItemsOpj = [
+    { title: "My Day", icon: <MyDaySVG />, link: "/todos/my-day" },
+    { title: "Work", icon: <WorkSVG />, link: "/todos/work" },
+    { title: "Important", icon: <ImportantSVG />, link: "/todos/important" },
+  ];
+
   return (
     <nav
       aria-label="side navigation"
@@ -14,71 +24,23 @@ export default function CustomCategories() {
     >
       <div>
         <ul className="flex flex-col flex-1 gap-1 py-3 ">
-          <li className="px-3">
-            <NavLink
-              to="/todos/all"
-              className="flex items-center gap-3 rounded p-3 text-gray-100 transition-colors  hover:text-[#0084ff]  aria-[current=page]:bg-[#283541] aria-[current=page]:text-gray-100 "
-            >
-              <div className="flex items-center self-center w-6">
-                <AllTasksSVG />
-              </div>
-              <div className="flex flex-col items-start justify-center flex-1 w-full gap-0 overflow-hidden text-sm truncate">
-                All Tasks
-              </div>
-            </NavLink>
-          </li>
-          <li className="px-3">
-            <NavLink
-              to="/todos/my-day"
-              className="flex items-center gap-3 rounded p-3 text-gray-100 transition-colors  hover:text-[#0084ff]  aria-[current=page]:bg-[#283541] aria-[current=page]:text-gray-100 "
-            >
-              <div className="flex items-center self-center w-6">
-                <MyDaySVG />
-              </div>
-              <div className="flex flex-col items-start justify-center flex-1 w-full gap-0 overflow-hidden text-sm truncate">
-                My Day
-              </div>
-            </NavLink>
-          </li>
-          {/* ALLLL Tasks */}
-          <li className="px-3">
-            <NavLink
-              to="/todos/work"
-              className="flex items-center gap-3 rounded p-3 text-gray-100 transition-colors  hover:text-[#0084ff]  aria-[current=page]:bg-[#283541] aria-[current=page]:text-gray-100 "
-            >
-              <div className="flex items-center self-center w-6">
-                <WorkSVG />
-              </div>
-              <div className="flex flex-col items-start justify-center flex-1 w-full gap-0 overflow-hidden text-sm truncate">
-                Work
-              </div>
-            </NavLink>
-          </li>
-          <li className="px-3">
-            <NavLink
-              to="/todos/home"
-              className="flex items-center gap-3 rounded p-3 text-gray-100 transition-colors  hover:text-[#0084ff]  aria-[current=page]:bg-[#283541] aria-[current=page]:text-gray-100 "
-            >
-              <div className="flex items-center self-center w-6">
-                <HomeSVG />
-              </div>
-              <div className="flex flex-col items-start justify-center flex-1 w-full gap-0 overflow-hidden text-sm truncate">
-                Home
-              </div>
-            </NavLink>
-          </li>
-
-          <li className="px-3">
-            <NavLink
-              to="/todos/important"
-              className="flex items-center gap-3 rounded p-3 text-gray-100 transition-colors  hover:text-[#0084ff]  aria-[current=page]:bg-[#283541] aria-[current=page]:text-gray-100 "
-            >
-              <ImportantSVG />
-              <div className="flex flex-col items-start justify-center flex-1 w-full gap-0 overflow-hidden text-sm truncate">
-                Important
-              </div>
-            </NavLink>
-          </li>
+          {navItemsOpj.map((navItem) => {
+            return (
+              <li className="px-3">
+                <NavLink
+                  to={navItem.link}
+                  className="flex items-center gap-3 rounded p-3 text-gray-100 transition-colors  hover:text-[#0084ff]  aria-[current=page]:bg-[#283541] aria-[current=page]:text-gray-100 "
+                >
+                  <div className="flex items-center self-center w-6">
+                    {navItem.icon}
+                  </div>
+                  <div className="flex flex-col items-start justify-center flex-1 w-full gap-0 overflow-hidden text-sm truncate">
+                    {navItem.title}
+                  </div>
+                </NavLink>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </nav>
