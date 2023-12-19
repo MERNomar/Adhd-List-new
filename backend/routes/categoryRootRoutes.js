f5e71b5f15495b92ccc51c24e21f212d74c4adbf
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2023-12-19 04:20:11 +0200

    Added auth to rootRoutes

--- backend/routes/categoryRootRoutes.js
+++ backend/routes/categoryRootRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const categoryRootControllers = require("../controllers/categoryRootControllers");
 const authMiddleware = require("../middleware/authMIddleware");
 
+router.use(authMiddleware);
+
 router.post("/post-category-root", categoryRootControllers.postCategoryRoot);
 router.get("/get-category-root", categoryRootControllers.getCategoryRoots);
 
