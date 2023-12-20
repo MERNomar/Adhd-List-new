3f4dee3a6992ddc63e06fbbe9da80f0cdbce5bf9
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2023-12-20 03:04:56 +0200

    Map todos based on RootCategory

--- backend/controllers/categoryRootControllers.js
+++ backend/controllers/categoryRootControllers.js
@@ -3,14 +3,13 @@ const { CategoryRoot } = require("../models/categoryRootsModel");
 const postCategoryRoot = (req, res) => {
   const data = req.body;
   if (data.title.length === 6)
-    return res.status(400).json({ error: "Please enter the title" });
+    return res.status(400).json({ error: "Title is too short" });
   CategoryRoot.create(data)
     .then((data) => res.status(200).json(data))
     .catch((err) => res.status(400).json({ error: err }));
 };
 
 const getCategoryRoots = (req, res) => {
-  console.log(req);
   CategoryRoot.find()
     .then((data) => res.status(200).json(data))
     .catch((err) => res.status(400).json({ error: err }));
--- frontend/src/components/TodoList/TodoMangement/MapTodos.jsx
+++ frontend/src/components/TodoList/TodoMangement/MapTodos.jsx
@@ -3,12 +3,13 @@ import { useStore } from "../../../store/todoState";
 import { useParams } from "react-router-dom";
 
 export default function AllTodos() {
-  const { title: category } = useParams();
+  const { title: category, child: rootCategory } = useParams();
   const data = useStore((state) => state.allTasks);
   const arrayFilter = data.filter((item) => {
     if (item.completed) return;
-    if (category === "all") return item;
-    return item.category === category;
+    if (rootCategory) {
+      if (rootCategory === item.root_category) return item;
+    } else return category === item.category;
   });
   const categoryFilter = arrayFilter.reverse();
 
