814461147196a4c37fb3c7c6cc6507875b7c43c8
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2024-01-03 06:52:50 +0200

    Basic animated modal

--- frontend/src/components/TodoList/UserPanel/UserPanel.jsx
+++ frontend/src/components/TodoList/UserPanel/UserPanel.jsx
@@ -2,7 +2,7 @@ import { useUserPanel } from "../../../store/todoState";
 import { motion } from "framer-motion";
 export default function UserPanelNavBar() {
   const setIsUserPanelOpen = useUserPanel((store) => store.setIsUserPanelOpen);
-
+  const isUserPanelOpen = useUserPanel((store) => store.isUserPanelOpen);
   return (
     <motion.div
       onClick={() => {
@@ -11,13 +11,35 @@ export default function UserPanelNavBar() {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      className="w-full h-full z-[80] flex fixed top-0 bottom-0 left-0 right-0 bg-black/70 transition-100 transition-colors"
+      className={`
+      ${!isUserPanelOpen && "hidden"}
+      w-full h-full z-[80] flex fixed top-0 bottom-0 left-0 right-0 bg-black/70 transition-100 transition-colors`}
     >
       <motion.div
         onClick={(e) => {
           e.stopPropagation();
         }}
-        className="mx-auto my-auto text-white bg-black rounded w-[70%] h-[70%] 2xl:w-full  "
+        className="mx-auto my-auto text-white bg-black rounded w-[97%] h-[70%] sm:w-[70%] transition-all "
+        initial={{
+          opacity: 0,
+          scale: 0.75,
+        }}
+        animate={{
+          opacity: 1,
+          scale: 1,
+          transition: {
+            ease: "easeOut",
+            duration: 0.15,
+          },
+        }}
+        exit={{
+          opacity: 0,
+          scale: 0.75,
+          transition: {
+            ease: "easeIn",
+            duration: 0.15,
+          },
+        }}
       ></motion.div>
     </motion.div>
   );
