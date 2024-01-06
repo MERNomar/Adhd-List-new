2c37068accbc8364110e94c5c4f4dd9e86eb8009
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2024-01-06 07:38:45 +0200

    Added workDay model

---
+++ backend/models/workDayModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const workDaySchema = new Schema(
+  {
+    user_id: { type: String, required: true },
+    date: { type: String, required: true },
+    time_worked: { type: String, required: true },
+    commits_made: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const WorkDay = mongoose.model("work_day", workDaySchema);
+
+module.exports = { WorkDay };
