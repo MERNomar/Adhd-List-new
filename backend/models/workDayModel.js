5a63f286a7a082c2fc441288ac0748f65f882fb5
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2024-01-08 06:16:12 +0200

    Updated workDay model

--- backend/models/workDayModel.js
+++ backend/models/workDayModel.js
@@ -6,7 +6,7 @@ const workDaySchema = new Schema(
     user_id: { type: String, required: true },
     date: { type: String, required: true },
     time_worked: { type: String, required: true },
-    commits_made: { type: String, required: true },
+    commits_made: { type: Array, required: true },
   },
   { timestamps: true }
 );
